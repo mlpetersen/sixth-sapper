7234a820102cdc6fa30a246190a22f342457ce7b
Michael L Petersen <ekmlpe@danskespil.dk>
2020-03-28 10:41:24 +0100

    fade in bg

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,15 @@
 <script>
   import { onMount } from "svelte";
-  let bgHeight = 0;
-  let bgWidth = 0;
+  let bg = "";
+  let bgAnimation = "";
+  let blurredBg = `//res.cloudinary.com/sixth/image/upload/c_fill,e_blur:500,q_auto,w_500/v1585348748/bg.jpg`;
 
   onMount(() => {
     function resize() {
-      bgWidth = document.body.offsetWidth;
-      bgHeight = document.body.offsetHeight;
+      const bgWidth = document.body.offsetWidth;
+      const bgHeight = document.body.offsetHeight;
+      bg = `//res.cloudinary.com/sixth/image/upload/c_crop,g_center,f_auto,h_${bgHeight},q_auto,w_${bgWidth}/v1585348748/bg.webp`;
+      bgAnimation = "animate-in";
     }
     window.onresize = resize;
     resize();
@@ -49,6 +52,21 @@
     z-index: -1;
     background-size: cover;
     background-position: center;
+    background-image: url({blurredBg});
+  }
+
+  .bg .inner {
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+    transition: opacity 4s ease-out;
+    transition-delay: 200ms;
+    opacity: 0;
+  }
+
+  .bg.animate-in .inner {
+    opacity: 1;
   }
 </style>
 
@@ -71,6 +89,6 @@
   </figure>
 </div>
 
-<div
-  class="bg"
-  style="background-image:url(//res.cloudinary.com/sixth/image/upload/c_crop,g_center,f_auto,h_{bgHeight},q_auto,w_{bgWidth}/v1585348748/bg.webp)" />
+<div class="bg {bgAnimation}" style="background-image:url({blurredBg})">
+  <div class="inner" style="background-image:url({bg});" />
+</div>
